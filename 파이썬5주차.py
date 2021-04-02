d0950fa7c9f5bd9c4a8ab2d42a02e6fde09d3281
GyuriKim12 <kralicia@naver.com>
2021-04-03 02:32:09 +0900

    commit

--- 파이썬5주차.py
+++ 파이썬5주차.py
@@ -42,3 +42,19 @@ def fun(count):
 a=0
 print('result=',fun(count))
 print(a)
+
+#2개의 파라미터를 지정하여 지정한 파라미터 두개의 곱을 리턴하는 함수를 작성해라
+def print_numbers(a,b):
+    result=a*b
+    return result
+
+print_numbers(10, 5)
+
+#월급 계산 프로그램 만들기
+#근무 시간이 40시간을 초과하면 초과 근무 시간에는 시급의 1.5배를 지급한다
+def computepay(hour,rate):
+    if hour>40:
+        print('pay :',40*int(rate)+(int(hour)-40)*1.5*int(rate))
+    else:
+        print('pay =',int(hour)*int(rate))
+coumptupay(45,10)
